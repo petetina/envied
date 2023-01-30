f6bc81f05a539dd75493b4039052e003fb9e8e6a
Antoine Petetin <apetetin.prestataire@clauger.fr>
2023-01-30 12:31:52 +0100

    fix

--- packages/envied_generator/lib/src/generator.dart
+++ packages/envied_generator/lib/src/generator.dart
@@ -36,7 +36,7 @@ class EnviedGenerator extends GeneratorForAnnotation<EnviedMultiple> {
           final config = Envied(
             path: env.getField('path')?.toStringValue(), //.literalValue as String?,
             requireEnvFile: env.getField('requireEnvFile')?.toBoolValue() ?? false,
-            name: env.getField('name')?.toStringValue(),
+            name: env.getField('name').toString(),
             obfuscate: env.getField('obfuscate')?.toBoolValue() ?? false,
           );
 
